3fb6ea5da00fa7403feedf6ddc3c2bd348036442
HIENLTH @ HCMUE <hyhien@gmail.com>
2023-09-27 08:50:03 +0700

    Upload multiple files

--- MVCLab/MVCLab/Controllers/FileUploadController.cs
+++ MVCLab/MVCLab/Controllers/FileUploadController.cs
@@ -26,8 +26,32 @@ namespace MVCLab.Controllers
                 }
                 TempData["Message"] = $"Upload thành công {description}";
             }
-            catch {
+            catch
+            {
                 TempData["Message"] = $"Upload không thành công {description}";
+            }
+
+            return Redirect("Index");
+        }
+
+        [HttpPost]
+        public IActionResult UploadFiles(List<IFormFile> myfiles)
+        {
+            try
+            {
+                foreach (var myfile in myfiles)
+                {
+                    var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "MyFiles", myfile.FileName);
+                    using (var file = new FileStream(fullPath, FileMode.Create))
+                    {
+                        myfile.CopyTo(file);
+                    }
+                }
+                TempData["Message"] = $"Upload thành công";
+            }
+            catch
+            {
+                TempData["Message"] = $"Upload không thành công ";
             }
 
             return Redirect("Index");
