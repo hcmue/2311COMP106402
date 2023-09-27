0466cdcfa1b19cbeee354335581782e38ffae308
HIENLTH @ HCMUE <hyhien@gmail.com>
2023-09-27 08:13:48 +0700

    Demo upload single file

--- MVCLab/MVCLab/Controllers/FileUploadController.cs
+++ MVCLab/MVCLab/Controllers/FileUploadController.cs
@@ -8,5 +8,29 @@ namespace MVCLab.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult UploadFile(IFormFile myfile, string description)
+        {
+            try
+            {
+                var myPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "MyFiles");
+                if (!Directory.Exists(myPath))
+                {
+                    Directory.CreateDirectory(myPath);
+                }
+                var fullPath = Path.Combine(myPath, myfile.FileName);
+                using (var file = new FileStream(fullPath, FileMode.Create))
+                {
+                    myfile.CopyTo(file);
+                }
+                TempData["Message"] = $"Upload thành công {description}";
+            }
+            catch {
+                TempData["Message"] = $"Upload không thành công {description}";
+            }
+
+            return Redirect("Index");
+        }
     }
 }
